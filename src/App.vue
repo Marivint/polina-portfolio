c552ace19a010ddd2b90d7a2b368157442bd1a37
Yvann <marivint.yvann@gmail.com>
2020-11-22 00:40:32 +0100

    [DEV] Initial vue 3 + router

---
+++ src/App.vue
@@ -0,0 +1,10 @@
+<template>
+  <div id="app">
+    <div id="nav">
+      <router-link :to="{ name: 'Home' }">Home</router-link> |
+      <router-link :to="{ name: 'About' }">About</router-link>
+    </div>
+    <router-view />
+  </div>
+</template>
+
---
+++ src/router/index.js
@@ -0,0 +1,42 @@
+/* Import
+============================================= */
+import {
+  createRouter,
+  createWebHistory
+} from "vue-router";
+import vHome from "@/views/home.vue";
+import vAbout from "@/views/about.vue";
+import vNotFound from "@/views/404.vue";
+
+/* Routes
+============================================= */
+const routes = [{
+    path: "/",
+    name: "Home",
+    _component: vHome,
+    get component() {
+      return this._component;
+    },
+    set component(value) {
+      this._component = value;
+    },
+  },
+  {
+    path: "/about",
+    name: "About",
+    component: vAbout,
+  },
+  {
+    path: "/:catchAll(.*)",
+    component: vNotFound,
+  },
+];
+
+/* Router
+============================================= */
+const router = createRouter({
+  history: createWebHistory(),
+  routes,
+});
+
+export default router;
