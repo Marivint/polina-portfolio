47389962e4bc8cdbd55a21eea0972c83910c5671
Yvann <marivint.yvann@gmail.com>
2020-11-28 12:07:19 +0100

    [DEV] Enable parallax on all section

--- src/App.vue
+++ src/App.vue
@@ -1,72 +1,72 @@
 <template>
-  <!-- Header -->
-  <header>
-    <div class="container-fluid">
-      <nav class="row header-center">
-        <div class="col-10">
-          <div class="d-inline header-item-logo">
-            <!-- <logo class="header-logo"></logo> -->
-            Polina Gusarova
-          </div>
+  <div class="parallax">
+    <header>
+      <div class="container-fluid">
+        <nav class="row header-center">
+          <div class="col-10">
+            <div class="d-inline header-item-logo">
+              <!-- <logo class="header-logo"></logo> -->
+              Polina Gusarova
+            </div>
 
-          <div class="d-inline header-item header-item-grey">
-            <!-- <img src="@/assets/icon/chart.svg" /> -->
-            About
-          </div>
+            <div class="d-inline header-item header-item-grey">
+              <!-- <img src="@/assets/icon/chart.svg" /> -->
+              About
+            </div>
 
-          <div class="d-inline header-item header-item-grey">
-            Work
-          </div>
+            <div class="d-inline header-item header-item-grey">
+              Work
+            </div>
 
-          <div class="d-inline header-item header-item-grey">
-            Contact
+            <div class="d-inline header-item header-item-grey">
+              Contact
+            </div>
+          </div>
+          <div class="col text-right">
+            <button class="header-burger"></button>
+          </div>
+        </nav>
+      </div>
+    </header>
+
+    <!-- Router view -->
+    <router-view />
+    <!-- End router view -->
+
+    <footer>
+      <div class="container-fluid">
+        <div class="row">
+          <div class="col-12"><hr /></div>
+          <div class="col-2 offset-md-1">
+            <span class="project-cta subtitle subtitle-grey subtitle-left"
+              >About me
+            </span>
+          </div>
+          <div class="col-2">
+            <span class="project-cta subtitle subtitle-grey subtitle-left"
+              >My projects
+            </span>
+          </div>
+          <div class="col-2">
+            <span class="project-cta subtitle subtitle-grey subtitle-left"
+              >Contact me
+            </span>
           </div>
         </div>
-        <div class="col text-right">
-          <button class="header-burger"></button>
-        </div>
-      </nav>
-    </div>
-  </header>
-  <!-- End header -->
-
-  <!-- Router view -->
-  <router-view />
-  <!-- End router view -->
-
-  <footer>
-    <div class="container-fluid">
-      <div class="row">
-        <div class="col-12"><hr /></div>
-        <div class="col-2 offset-md-1">
-          <span class="project-cta subtitle subtitle-grey subtitle-left"
-            >About me
-          </span>
-        </div>
-        <div class="col-2">
-          <span class="project-cta subtitle subtitle-grey subtitle-left"
-            >My projects
-          </span>
-        </div>
-        <div class="col-2">
-          <span class="project-cta subtitle subtitle-grey subtitle-left"
-            >Contact me
-          </span>
+        <div class="row justify-content-around" id="footer-copyright">
+          <div class="col-4 text-left" id="copyright">
+            &copy; Copyright 2020, Polina Gusarova.
+          </div>
+          <div class="col-4 text-right">
+            Made by
+            <a href="#"
+              ><img id="logo-ym" src="@/assets/logos/logo-ym.png" alt=""
+            /></a>
+          </div>
         </div>
       </div>
-      <div class="row justify-content-around" id="footer-copyright">
-        <div class="col-4 text-left" id="copyright">
-          &copy; Copyright 2020, Polina Gusarova.
-        </div>
-        <div class="col-4 text-right">
-          Made by
-          <a href="#"
-            ><img id="logo-ym" src="@/assets/logos/logo-ym.png" alt=""
-          /></a>
-        </div>
-      </div>
-    </div>
-  </footer>
+    </footer>
+  </div>
 </template>
 
 <script>
--- src/views/homev2.vue
+++ src/views/homev2.vue
@@ -1,75 +1,199 @@
 <template>
-  <div class="parallax">
-    <div id="group1" class="parallax__group">
-      <div class="parallax__layer parallax__layer--base">
-        <div class="title">Base Layer</div>
+  <div id="about" class="parallax__group">
+    <div class="parallax__layer parallax__layer--base">
+      <!-- <section id="about"> -->
+      <div class="container-fluid">
+        <div class="row justify-content-md-center">
+          <div id="about-text" class="col-8">
+            <h1 class="user-select-none">
+              I am a junior <br />Production Manager.
+            </h1>
+
+            <p>
+              Passionate about creating and development of cultural projects, I
+              bring my managerial and artistic skills acquired during my
+              international career.
+            </p>
+          </div>
+        </div>
+      </div>
+      <!-- </section> -->
+    </div>
+  </div>
+  <div id="skills" class="parallax__group">
+    <div class="parallax__layer parallax__layer--base">
+      <!-- <section id="skills"> -->
+      <div class="container-fluid">
+        <div class="row align-items-center">
+          <div class="col-4"><h2>Production</h2></div>
+
+          <div class="col-4"><h2>Communication</h2></div>
+
+          <div class="col-4"><h2>Creation</h2></div>
+        </div>
+      </div>
+      <!-- </section> -->
+    </div>
+  </div>
+  <div id="scroll-down" class="parallax__group">
+    <div class="parallax__layer parallax__layer--base">
+      <div class="container-fluid">
+        <div class="row justify-content-md-center">
+          <div class="col-1">
+            <i class="btn-icon-round">
+              <icon-arrow-down
+                colorClass="custom-icon-black"
+                otherClass="btn-round"
+                height="30"
+                width="30"
+              />
+            </i>
+          </div>
+        </div>
       </div>
     </div>
-    <div id="group2" class="parallax__group">
-      <div class="parallax__layer parallax__layer--base">
-        <div class="title">Base Layer</div>
-      </div>
-      <div class="parallax__layer parallax__layer--back">
-        <div class="title">Background Layer</div>
-      </div>
-    </div>
-    <div id="group3" class="parallax__group">
-      <div class="parallax__layer parallax__layer--fore">
-        <div class="title">Foreground Layer</div>
-      </div>
-      <div class="parallax__layer parallax__layer--base">
-        <div class="title">Base Layer</div>
-      </div>
-    </div>
-    <div id="group4" class="parallax__group">
-      <div class="parallax__layer parallax__layer--base">
-        <div class="title">Base Layer</div>
-      </div>
-      <div class="parallax__layer parallax__layer--back">
-        <div class="title">Background Layer</div>
-      </div>
-      <div class="parallax__layer parallax__layer--deep">
-        <div class="title">Deep Background Layer</div>
+  </div>
+  <div id="projects" class="parallax__group">
+    <div class="parallax__layer parallax__layer--base">
+      <div class="container-fluid">
+        <project
+          format="left"
+          page="1-7"
+          title="Lambinet Museum in complete privacy"
+          :subtitles="['Conception', 'Management', 'Production']"
+          :src="require('@/assets/projects/placeholder-400x500.png')"
+        ></project>
+
+        <project
+          format="right"
+          page="1-7"
+          title="Lambinet Museum in complete privacy"
+          :subtitles="['Conception', 'Management', 'Production']"
+          :src="require('@/assets/projects/placeholder-400x500.png')"
+        ></project>
       </div>
     </div>
-    <div id="group5" class="parallax__group">
-      <div class="parallax__layer parallax__layer--fore">
-        <div class="title">Foreground Layer</div>
-      </div>
-      <div class="parallax__layer parallax__layer--base">
-        <div class="title">Base Layer</div>
-      </div>
+    <!-- <div class="parallax__layer parallax__layer--back">
+      <div class="title">Background Layer</div>
     </div>
-    <div id="group6" class="parallax__group">
-      <div class="parallax__layer parallax__layer--back">
-        <div class="title">Background Layer</div>
-      </div>
-      <div class="parallax__layer parallax__layer--base">
-        <div class="title">Base Layer</div>
-      </div>
-    </div>
-    <div id="group7" class="parallax__group">
-      <div class="parallax__layer parallax__layer--base">
-        <div class="title">Base Layer</div>
+    <div class="parallax__layer parallax__layer--deep">
+      <div class="title">Deep Background Layer</div>
+    </div> -->
+  </div>
+  <div id="contact" class="parallax__group">
+    <div class="parallax__layer parallax__layer--base">
+      <div class="container-fluid">
+        <div class="row justify-content-center align-items-center">
+          <div id="contact-card" class="col-9">
+            <div class="row">
+              <div class="col-6">
+                <span class="project-cta subtitle subtitle-white subtitle-left"
+                  >Contact me
+                </span>
+
+                <br />
+
+                <h2>Let's Work <br />Together.</h2>
+
+                <br />
+
+                <p>
+                  Lorem ipsum, dolor sit amet consectetur adipisicing elit.
+                  Deleniti, esse architecto dolorum alias iusto ab voluptatum,
+                  assumenda natus.
+                </p>
+              </div>
+              <div class="col-6 text-center"></div>
+            </div>
+            <form id="contact-form" class="row">
+              <div class="col-6">
+                <span class="svg-text">
+                  <icon-message
+                    colorClass="custom-icon-white"
+                    otherClass=""
+                    height="30"
+                    width="30"
+                  />
+                  <div class="text-wrapper">gusarova@gmail.com</div>
+                </span>
+
+                <span class="svg-text">
+                  <icon-location
+                    colorClass="custom-icon-white"
+                    otherClass=""
+                    height="30"
+                    width="30"
+                  />
+                  <div class="text-wrapper">Charenton-le-pont</div>
+                </span>
+              </div>
+              <div class="col-6">
+                <div class="form-label-group">
+                  <input
+                    type="text"
+                    id="inputName"
+                    class="form-control"
+                    placeholder="Name"
+                    required
+                  />
+                </div>
+
+                <br />
+
+                <div class="form-label-group">
+                  <input
+                    type="email"
+                    id="inputEmail"
+                    class="form-control"
+                    placeholder="Email address"
+                    required
+                  />
+                </div>
+
+                <br />
+
+                <textarea
+                  class="form-control"
+                  id="inputMessage"
+                  placeholder="Your message"
+                  rows="10"
+                  required
+                ></textarea>
+
+                <br />
+
+                <button class="btn btn-blue col-8">
+                  Send
+                  <icon-arrow-right
+                    colorClass="custom-icon-white"
+                    otherClass=""
+                    height="30"
+                    width="30"
+                  />
+                </button>
+              </div>
+            </form>
+          </div>
+        </div>
       </div>
     </div>
   </div>
 </template>
 
 <script>
-// import project from "../components/project/project.vue";
+import project from "../components/project/project.vue";
 // import iconMessage from "../components/icons/icon-message.vue";
 // import iconLocation from "../components/icons/icon-location.vue";
 // import iconArrowRight from "../components/icons/icon-arrow-right.vue";
-// import iconArrowDown from "../components/icons/icon-arrow-down.vue";
+import iconArrowDown from "../components/icons/icon-arrow-down.vue";
 
 export default {
   components: {
-    // project,
+    project,
     // iconMessage,
     // iconLocation,
     // iconArrowRight,
-    // iconArrowDown
+    iconArrowDown
   }
 };
 </script>
